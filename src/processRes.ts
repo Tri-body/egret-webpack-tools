1e003b32025a64e3ee76aa8d97f9a9b914eeaca9
gnoijli <gnoijli@gmail.com>
2021-05-18 15:29:08 +0800

    skip process resource if not exist

--- src/processRes.ts
+++ src/processRes.ts
@@ -10,9 +10,14 @@ const BASE_NAME = '[name].[hash:8].[ext]'
 
 export async function processRes(compiler: Compiler, compilation: compilation.Compilation, options: IOptions) {
   const context = options.context || compiler.context
-  const resBaseName = options.assetsName || BASE_NAME
   const resPath = path.join(context, options.resource || DEFAULT_RES_PATH)
-  const resJson = await fs.readJson(path.join(resPath, DEFAULT_RES_NAME))
+  const resJsonPath = path.join(resPath, DEFAULT_RES_NAME)
+  if (!fs.existsSync(resJsonPath)) {
+    console.log('no egret res cfg, skip...')
+    return
+  }
+  const resBaseName = options.assetsName || BASE_NAME
+  const resJson = await fs.readJson(resJsonPath)
   const resources = resJson.resources || []
   let buffer: Buffer, json: any, outputName: string, outputName2: string, fileName: string, fileName2: string, filePath: string, filePath2: string
   for (let r of resources) {
